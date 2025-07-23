986b7c22109146cab30204d7d91bc7a7d44f5865
Ramkishore <ramkishore1706@gmail.com>
2025-07-23 06:43:39 +0530

    Updated table design and added content in About section

--- Backend/server.js
+++ Backend/server.js
@@ -43,6 +43,35 @@ app.get("/api/show",(req,res)=>{
     res.json(sendData);
 });
 
+app.delete("/api/delete",(req,res)=>{
+    const indexData = req.body;
+    console.log("Index : "+indexData.id);
+
+    const readData = fs.readFileSync("customers.json","utf-8");
+    const datum = JSON.parse(readData);
+    
+    if(indexData.id === 0){
+        datum.splice(0,0);
+    }
+    else{
+        datum.splice(indexData.id,indexData.id);
+    }
+
+    fs.writeFileSync('customers.json',JSON.stringify(datum,null,2));
+
+    
+    res.json({message:"Success"});
+
+});
+
+//To categorize which leads to sorting
+app.get("/api/category",(req,res)=>{
+    const jsonData = fs.readFileSync("customers.json",'utf-8');
+    const datum = JSON.parse(jsonData);
+
+    console.log(datum)
+    res.json()
+})
 //To enable the port to listen to the server
 app.listen(port ,()=>{
     console.log(`Server is running in ${port} successfully!!`);
